7092dc81ec133f18191d8a0e07b1a773275463d9
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-09-29 20:56:20 -0300

    creating the form

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,6 +14,16 @@ const Navbar = () => {
           </NavLink>
         </li>
         <li>
+          <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>
+            Entrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>
+            Registrar-se
+          </NavLink>
+        </li>
+        <li>
           <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : "")}>
             About
           </NavLink>
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,9 +1,31 @@
 import styles from "./Register.module.css"
 
+import { useState, useEffect } from "react"
+
 const Register = () => {
   return (
     <div>
-      
+      <h1>Efetue o registro para criar seus próprios posts</h1>
+      <p>Crie seu usuário e compartilhe suas experiências</p>
+      <form>
+        <label>
+          {/* <span>Nome:</span> */}
+          <input type="text" name="displayName" required placeholder="Nome de usuário"/>
+        </label>
+        <label>
+          {/* <span>E-mail:</span> */}
+          <input type="email" name="email" required placeholder="E-mail"/>
+        </label>
+        <label>
+          {/* <span>Senha:</span> */}
+          <input type="password" name="password" required placeholder="Insira sua senha"/>
+        </label>
+        <label>
+          {/* <span>Senha:</span> */}
+          <input type="password" name="confirmPassword" required placeholder="Confirme sua senha"/>
+        </label>
+        <button className="btn">Registrar</button>
+      </form>
     </div>
   )
 }
