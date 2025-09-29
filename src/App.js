fe6a68ac7246961a82699f7fa9b824316327d2c4
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-09-29 20:48:07 -0300

    Adding login and register pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
 
 // pages
 
@@ -16,6 +20,8 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />}>Home</Route>
             <Route path='/about' element={<About />}>Sobre</Route>
+            <Route path='/register' element={<Register/>}></Route>
+            <Route path='/login' element={<Login/>}></Route>
           </Routes>
         </div>
         <Footer/>
